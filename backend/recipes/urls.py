6478ad18752da90086d09e7096ede42ad76ba42b
Henrique Salgueiro <henriquehssjc@gmail.com>
2024-08-05 15:05:03 -0300

    adding (optional) format suffixes to the urls

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from recipes import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
   path('', views.recipe_view),
   path('/<int:pk>', views.recipe_details_view),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns) # Com isso, o cliente pode especificar o formato da resposta na URL (.json, .xml, etc)
 
