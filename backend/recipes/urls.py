adcfd4d723c7c029bc4689a9cab90921ed497f58
Henrique Salgueiro <henriquehssjc@gmail.com>
2024-08-06 10:25:14 -0300

    using class based views instead of function based views.

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -3,8 +3,8 @@ from recipes import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-  path('', views.recipe_view),
-  path('/<int:pk>', views.recipe_details_view),
+  path('', views.RecipeView.as_view()),
+  path('/<int:pk>', views.RecipeDetailsView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns) # Com isso, o cliente pode especificar o formato da resposta na URL (.json, .xml, etc)
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,40 +1,44 @@
+from django.http import Http404
 from recipes.models import Recipe
 from recipes.serializers import RecipeSerializer 
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-@api_view(['GET', 'POST'])
-def recipe_view(request, format=None):
-  if request.method == 'GET':
+class RecipeView(APIView):
+  def get(self, request, format=None):
     recipes = Recipe.objects.all()
-    serializer = RecipeSerializer(recipes, many=True) # translates Native into Python
+    serializer = RecipeSerializer(recipes, many=True)
     return Response(serializer.data)
-  elif request.method == 'POST':
+
+  def post(sel, request, format=None):
     serializer = RecipeSerializer(data=request.data)
     if serializer.is_valid():
       serializer.save()
       return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def recipe_details_view(request, pk, format=None):
-  try:
-    recipe = Recipe.objects.get(pk=pk)
-  except Recipe.DoesNotExist:
-    return Response(status=status.HTTP_404_NOT_FOUND)
-  
-  if request.method == 'GET':
+class RecipeDetailsView(APIView):
+  def get_object(self, pk):
+    try:
+      return Recipe.objects.get(pk=pk)
+    except Recipe.DoesNotExist:
+      raise Http404
+
+  def get(self, request, pk, format=None):
+    recipe = self.get_object(pk)
     serializer = RecipeSerializer(recipe)
     return Response(serializer.data)
   
-  elif request.method == 'PUT':
+  def put(self, request, pk, format=None):
+    recipe = self.get_object(pk)
     serializer = RecipeSerializer(recipe, data=request.data)
     if serializer.is_valid():
       serializer.save()
       return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-  
-  elif request.method == 'DELETE':
+
+  def delete(self, request, pk, format=None):
+    recipe = self.get_object(pk)
     recipe.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
